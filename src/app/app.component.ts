a95d6182c0417a835e552393efb2b31a883e26ae
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-09-12 15:39:00 +0200

    Add Angular boilerplate

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'vigilant-broccoli';
+}
