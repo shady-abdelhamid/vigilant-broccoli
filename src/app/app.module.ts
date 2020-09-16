9cc09f01e1a7e84308c5114565292ce03f13464a
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-09-16 08:47:36 +0200

    Disable akita devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    // environment.production ? [] : AkitaNgDevtools.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
