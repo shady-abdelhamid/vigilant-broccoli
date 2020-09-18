bd44df7884503d9d26c4a5f2d0b3e2bc6dab976b
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-09-18 12:42:12 +0200

    Fix lint

--- src/app/heroes/hero-list/hero-list.component.spec.ts
+++ src/app/heroes/hero-list/hero-list.component.spec.ts
@@ -1,19 +1,18 @@
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { NgxSwitchInputModule } from "@ngx-tiny/switch-input/ngx-switch-input.module";
-import { HeroOrderPipe } from "../hero-order.pipe";
-import { HeroSearchPipe } from "../hero-search.pipe";
-import { HeroesService } from "../state/heroes.service";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroOrderPipe } from '../hero-order.pipe';
+import { HeroSearchPipe } from '../hero-search.pipe';
+import { HeroesService } from '../state/heroes.service';
 
-import { HeroListComponent } from "./hero-list.component";
+import { HeroListComponent } from './hero-list.component';
 
-describe("HeroListComponent", () => {
+describe('HeroListComponent', () => {
   let component: HeroListComponent;
   let fixture: ComponentFixture<HeroListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, NgxSwitchInputModule],
+      imports: [HttpClientTestingModule],
       declarations: [HeroListComponent, HeroSearchPipe, HeroOrderPipe],
       providers: [HeroesService],
     }).compileComponents();
@@ -25,7 +24,7 @@ describe("HeroListComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
